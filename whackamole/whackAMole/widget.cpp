70919d65eafa2ae7171c31dee8904bb3e7b1280f
3toyo3 <ytagle@bu.edu>
2023-04-24 22:57:42 -0400

    removed scrollbars

--- whackamole/whackAMole/widget.cpp
+++ whackamole/whackAMole/widget.cpp
@@ -1,22 +1,23 @@
 #include "widget.h"
 #include "ui_widget.h"
 # include <QDebug>
+#include <QScrollBar>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent), ui(new Ui::Widget){
     ui->setupUi(this);
 ;
 
-
-    this->setStyleSheet("QScrollBar:vertical {width: 0;}");
-    this->setStyleSheet("QScrollBar:horizontal {height: 0;}");
     ui->graphicsView->setStyleSheet("QScrollBar:horizontal {height: 0;}");
     ui->graphicsView->setStyleSheet("QScrollBar:vertical {width: 0;}");
+    //this->setStyleSheet("QScrollBar:vertical {width: 0;}");
+    this->setStyleSheet("QScrollBar:horizontal {height: 0;}");
 
 
     this->setAttribute(Qt::WA_AcceptTouchEvents, true);
 
     Scene *scene = new Scene();
+
     scene -> setSceneRect(0,0,480,272);
     Scene::selfptr = scene;
     QGraphicsPixmapItem *pixItem = new QGraphicsPixmapItem(QPixmap(":/images/bg.png"));
